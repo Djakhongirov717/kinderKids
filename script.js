90247f17ae7b445929fba9679bfb663a82244be9
Djakhongirov717 <129034144+Djakhongirov717@users.noreply.github.com>
2023-03-27 10:54:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let instagram = document.querySelector('.instagram')
+let telegram = document.querySelector('.telegram')
+let telephone = document.querySelector('.telephone')
+
+instagram.onclick = () =>{
+    window.location.href = "https://www.instagram.com/kinder_kids_samarqand/"
+}
+telegram.onclick = () =>{
+    window.location.href = "https://l.instagram.com/?u=https%3A%2F%2Ft.me%2Ffbsfa&e=AT2kPNOCtVNFC0YpNmK6Av90WvWgYsiG8g7u327wnhwMB3uEjutVedemJb03Tn6wwM1CH6Vt1SbvRnVRsGapqTAOTuBiTZ5e"
+}
+telephone.onclick = () =>{
+    window.location.href = "tel:+998331110006"
+}
+
+
